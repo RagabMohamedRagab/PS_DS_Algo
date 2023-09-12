5c627a148c2e5bb03af00bae9d9bcc17e2f16022
Rajab Mohamed <64719825+RagabMohamedRagab@users.noreply.github.com>
2023-09-12 11:31:30 +0300

    Count Charactres

---
+++ Juniors Phase 1 Training/B. Count characters/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Countcharacters
+{
+    public class Program
+    {
+        static string SortString(string input)
+        {
+            char[] characters = input.ToArray();
+            Array.Sort(characters);
+            return new string(characters);
+        }
+        private static void Main(string[] args)
+        {
+            string str = Console.ReadLine();
+            str = SortString(str);
+          var dict=new Dictionary<char,int>();
+          foreach (var item in str)
+          {
+            if(dict.ContainsKey(item)){
+                ++dict[item];
+            }else{
+                dict.Add(item,1);
+            }
+          }
+          foreach (var item in dict)
+          {
+            Console.WriteLine($"{item.Key} {item.Value}");
+          }
+        }
+    }
+}
