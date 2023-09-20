2e569c2c8058509fcee9778e9ec0ac791e5377b7
Rajab Mohamed <64719825+RagabMohamedRagab@users.noreply.github.com>
2023-09-20 16:28:51 +0300

    C.Sort The Array

---
+++ Juniors Phase 1 Training/C. Sort the array/Program.cs
@@ -0,0 +1,42 @@
+﻿// Happy Code
+// Time Complexity O(N)
+// Space Complexity O(N)
+using System;
+namespace SortArray
+{
+	public class Program
+	{
+		private static void Main(string[] args)
+		{
+			int size = Int32.Parse(Console.ReadLine());
+			string[] arr = Console.ReadLine().Split(' ');
+			int max = 0, min = 0;
+			for (int a = 0; a < size; a++)
+			{
+				int index = Int32.Parse(arr[a]);
+				if (max < index)
+				{
+					max = index;
+				}
+				else
+				{
+					min = index;
+				}
+			}
+			int[] Frq = new int[max + 1];
+			for (int b = 0; b < size; b++)
+			{
+				int index = Int32.Parse(arr[b]);
+				Frq[index]++;
+			}
+			for (int i = 1; i <= max; i++)
+			{
+				for (int j = 1; j < Frq[i]; j++)
+				{
+					Console.Write($"{i} ");
+				}
+			}
+
+		}
+	}
+}
